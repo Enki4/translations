37ba30f49cd0a19abaf94c4448773397ea57d55f
Enki4 <Enki4@users.noreply.github.com>
2014-06-10 23:06:54 +0200

    Minetest Game in Italiano

---
+++ minetest_game_ita/mods/default/craftitems.lua
@@ -0,0 +1,92 @@
+-- mods/default/craftitems.lua
+
+minetest.register_craftitem("default:stick", {
+	description = "Bastone",
+	inventory_image = "default_stick.png",
+	groups = {stick=1},
+})
+
+minetest.register_craftitem("default:paper", {
+	description = "Carta",
+	inventory_image = "default_paper.png",
+})
+
+minetest.register_craftitem("default:book", {
+	description = "Libro",
+	inventory_image = "default_book.png",
+})
+
+minetest.register_craftitem("default:coal_lump", {
+	description = "Pezzo di carbone",
+	inventory_image = "default_coal_lump.png",
+})
+
+minetest.register_craftitem("default:iron_lump", {
+	description = "Pezzo di ferro",
+	inventory_image = "default_iron_lump.png",
+})
+
+minetest.register_craftitem("default:copper_lump", {
+	description = "Pezzo di rame",
+	inventory_image = "default_copper_lump.png",
+})
+
+minetest.register_craftitem("default:mese_crystal", {
+	description = "Cristallo di mese",
+	inventory_image = "default_mese_crystal.png",
+})
+
+minetest.register_craftitem("default:gold_lump", {
+	description = "Pezzo di oro",
+	inventory_image = "default_gold_lump.png",
+})
+
+minetest.register_craftitem("default:diamond", {
+	description = "Diamante",
+	inventory_image = "default_diamond.png",
+})
+
+minetest.register_craftitem("default:clay_lump", {
+	description = "Pezzo di argilla",
+	inventory_image = "default_clay_lump.png",
+})
+
+minetest.register_craftitem("default:steel_ingot", {
+	description = "Lingotto di acciaio",
+	inventory_image = "default_steel_ingot.png",
+})
+
+minetest.register_craftitem("default:copper_ingot", {
+	description = "Lingotto di rame",
+	inventory_image = "default_copper_ingot.png",
+})
+
+minetest.register_craftitem("default:bronze_ingot", {
+	description = "Lingotto di bronzo",
+	inventory_image = "default_bronze_ingot.png",
+})
+
+minetest.register_craftitem("default:gold_ingot", {
+	description = "Lingotto di oro",
+	inventory_image = "default_gold_ingot.png"
+})
+
+minetest.register_craftitem("default:mese_crystal_fragment", {
+	description = "Frammento di cristallo di mese",
+	inventory_image = "default_mese_crystal_fragment.png",
+})
+
+minetest.register_craftitem("default:clay_brick", {
+	description = "Mattone di argilla",
+	inventory_image = "default_clay_brick.png",
+})
+
+minetest.register_craftitem("default:scorched_stuff", {
+	description = "Roba bruciata",
+	inventory_image = "default_scorched_stuff.png",
+})
+
+minetest.register_craftitem("default:obsidian_shard", {
+	description = "Scheggia di ossidiana",
+	inventory_image = "default_obsidian_shard.png",
+})
---
+++ minetest_game_ita/mods/wool/init.lua
@@ -0,0 +1,49 @@
+-- minetest/wool/init.lua
+
+-- Backwards compatibility with jordach's 16-color wool mod
+minetest.register_alias("wool:dark_blue", "wool:blue")
+minetest.register_alias("wool:gold", "wool:yellow")
+
+local wool = {}
+-- This uses a trick: you can first define the recipes using all of the base
+-- colors, and then some recipes using more specific colors for a few non-base
+-- colors available. When crafting, the last recipes will be checked first.
+wool.dyes = {
+	{"white",      "bianca",      nil},
+	{"grey",       "grigia",       "basecolor_grey"},
+	{"black",      "nera",      "basecolor_black"},
+	{"red",        "rossa",        "basecolor_red"},
+	{"yellow",     "gialla",     "basecolor_yellow"},
+	{"green",      "verde",      "basecolor_green"},
+	{"cyan",       "turchese",       "basecolor_cyan"},
+	{"blue",       "blu",       "basecolor_blue"},
+	{"magenta",    "magenta",    "basecolor_magenta"},
+	{"orange",     "arancione",     "excolor_orange"},
+	{"violet",     "viola",     "excolor_violet"},
+	{"brown",      "marrone",      "unicolor_dark_orange"},
+	{"pink",       "rosa",       "unicolor_light_red"},
+	{"dark_grey",  "grigia scura",  "unicolor_darkgrey"},
+	{"dark_green", "verde scura", "unicolor_dark_green"},
+}
+
+for _, row in ipairs(wool.dyes) do
+	local name = row[1]
+	local desc = row[2]
+	local craft_color_group = row[3]
+	-- Node Definition
+	minetest.register_node("wool:"..name, {
+		description = "Lana "..desc,
+		tiles = {"wool_"..name..".png"},
+		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=3,flammable=3,wool=1},
+		sounds = default.node_sound_defaults(),
+	})
+	if craft_color_group then
+		-- Crafting from dye and white wool
+		minetest.register_craft({
+			type = "shapeless",
+			output = 'wool:'..name,
+			recipe = {'group:dye,'..craft_color_group, 'group:wool'},
+		})
+	end
+end
+
